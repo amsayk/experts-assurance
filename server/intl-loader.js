6d683d7a335005e0175689844c16847300dadaaf
Amadou CISSE <cisse.amadou.9@gmail.com>
2016-12-25 01:34:44 +0000

    Node locale support.

--- server/intl-loader.js
+++ server/intl-loader.js
@@ -1,5 +1,24 @@
 import { addLocaleData } from 'react-intl';
+import areIntlLocalesSupported from 'intl-locales-supported';
 const debug = require('debug')('app:server:intl');
+
+const localesMyAppSupports = [
+  'en'
+];
+
+if (global.Intl) {
+  // Determine if the built-in `Intl` has the locale data we need.
+  if (!areIntlLocalesSupported(localesMyAppSupports)) {
+    // `Intl` exists, but it doesn't have the data we need, so load the
+    // polyfill and patch the constructors we need with the polyfill's.
+    const IntlPolyfill    = require('intl');
+    Intl.NumberFormat     = IntlPolyfill.NumberFormat;
+    Intl.DateTimeFormat   = IntlPolyfill.DateTimeFormat;
+  }
+} else {
+  // No `Intl`, so use and load the polyfill.
+  global.Intl = require('intl');
+}
 
 const loaders = {
   en() {
