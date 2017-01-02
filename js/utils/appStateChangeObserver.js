d4ee804cd3d10acc0244aee8861f15d81e1d6de1
Amadou CISSE <cisse.amadou.9@gmail.com>
2017-01-02 16:50:29 +0000

    Fix auto logout.

--- js/utils/appStateChangeObserver.js
+++ js/utils/appStateChangeObserver.js
@@ -8,38 +8,36 @@ const log = require('log')('app:client:appStateChangeObserver');
 export default function appStateChangeObserver(store) {
   let timeout;
 
-  function doLogOut() {
+  function clearTimeout() {
     if (timeout) {
       window.clearTimeout(timeout);
       timeout = null;
     }
+  }
 
+  function doLogOut() {
+    clearTimeout();
     store.dispatch(logOut());
   }
 
   function cb() {
     log(`[APP STATE CHANGED]: ${AppState.currentState}`);
 
+    clearTimeout();
+
     // fires when user switches tabs, apps, goes to homescreen, etc.
     if (AppState.currentState === 'background') {
       timeout = window.setTimeout(doLogOut, INACTIVITY_TIMEOUT);
     }
-
-    // fires when app transitions from prerender, user returns to the app / tab.
-    if (AppState.currentState === 'active') {
-      if (timeout) {
-        window.clearTimeout(timeout);
-        timeout = null;
-      }
-    }
   }
-
-  cb();
 
   // subscribe to visibility change events
   AppState.addEventListener('change', cb);
   return {
-    remove: () => AppState.removeEventListener('change', cb),
+    remove: () =>  {
+      clearTimeout();
+      AppState.removeEventListener('change', cb)
+    },
   };
 }
 
