2fb4639694593697841fda73a5a6e16b82d059aa
Amadou CISSE <cisse.amadou.9@gmail.com>
2017-01-12 15:30:34 +0000

    Partially resolve Issue #4.

--- js/utils/auth/authWrappers/UserIsAuthenticated.js
+++ js/utils/auth/authWrappers/UserIsAuthenticated.js
@@ -2,6 +2,7 @@ import { PATH_LOGIN } from 'vars';
 import { UserAuthWrapper } from 'redux-auth-wrapper';
 
 export default UserAuthWrapper({
+  wrapperDisplayName     : 'UserIsAuthenticated',
   authSelector           : state => state.get('user').toJS(),
   failureRedirectPath    : PATH_LOGIN,
 });
