24f7b93c8ddc045db403a01129eb2f1a27300526
Amadou CISSE <cisse.amadou.9@gmail.com>
2017-01-17 23:29:11 +0000

    Log out -> Log Out.

--- js/routes/Landing/messages.js
+++ js/routes/Landing/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   logOut: {
     id: 'landing-page.logOut',
-    defaultMessage: 'Log out',
+    defaultMessage: 'Log Out',
   },
 
   manageAccount: {
